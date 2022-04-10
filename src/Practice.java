1acc280265e50f68338c5c9942cebaf7d0b28b71
YahyaKanibol <97798090+YahyaKanibol@users.noreply.github.com>
2022-04-10 18:21:56 +0300

    initial commit6

---
+++ src/Practice.java
@@ -0,0 +1,18 @@
+public class Practice {
+
+    public String name;
+    public int age;
+
+    public Practice(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+    public String toString() {
+        return "Practice{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
